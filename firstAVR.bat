6f6ddfb3372d20c32017face03835b6c3b839438
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2021-02-18 22:47:23 +0500

    First AVR assembly code that can flip the UNO's LED on or off

---
+++ firstAVR.bat
@@ -0,0 +1,4 @@
+avr-as -mmcu=atmega328p code.S -o code.o
+avr-objcopy -O ihex -R .eeprom code.o code.hex
+avr-objdump -mavr -D code.hex
+avrdude -F -V -c arduino -p ATMEGA328P -P com3 -b 115200 -U flash:w:code.hex
