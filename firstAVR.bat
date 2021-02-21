12ecdcdb7a1b1857bc0daa5667649e6dfa295aeb
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2021-02-21 19:13:30 +0500

    First 'Atom' commit

--- firstAVR.bat
+++ firstAVR.bat
@@ -1,4 +1,5 @@
 avr-as -mmcu=atmega328p code.S -o code.o
 avr-objcopy -O ihex -R .eeprom code.o code.hex
+avr-objdump -D code.o
 avr-objdump -mavr -D code.hex
 avrdude -F -V -c arduino -p ATMEGA328P -P com3 -b 115200 -U flash:w:code.hex
