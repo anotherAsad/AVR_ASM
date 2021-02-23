c11e9e14b46ab27b80fc51187a9a7a71a3e2bbc1
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2021-02-23 14:28:12 +0500

    label-addr translation working after using avr-ld.

--- firstAVR.bat
+++ firstAVR.bat
@@ -1,5 +1,6 @@
 avr-as -mmcu=atmega328p code.S -o code.o
-avr-objcopy -O ihex code.o code.hex
-avr-objdump -D code.o
-::avr-objdump -mavr -D code.hex
-avrdude -D -F -V -c arduino -p ATMEGA328P -P com3 -b 115200 -U flash:w:code.hex
+avr-ld code.o -o ldcode.o
+avr-objcopy -O ihex ldcode.o code.hex
+avr-objdump -D ldcode.o
+avr-objdump -mavr -D code.hex
+avrdude -D -F -V -c arduino -p ATMEGA328P -P com13 -b 115200 -U flash:w:code.hex
